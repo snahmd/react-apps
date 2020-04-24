cf3a73b8ce65dec0cb8f5012f603e8045c52d366
snahmd <sanahmed7@gmail.com>
2020-04-24 22:00:39 +0200

    using semantic ui

--- src/App.js
+++ src/App.js
@@ -2,9 +2,26 @@ import React from 'react'
 
 const App = () => {
   return(
-    <div>
-      Merhaba
+    <div class="ui comments">
+  <div class="comment">
+    <a class="avatar">
+      <img src="/images/avatar/small/stevie.jpg"/>
+    </a>
+    <div class="content">
+      <a class="author">Stevie Feliciano</a>
+      <div class="metadata">
+        <div class="date">2 days ago</div>
+        <div class="rating">
+          <i class="star icon"></i>
+          5 Faves
+        </div>
+      </div>
+      <div class="text">
+        Hey guys, I hope this example comment is helping you read this documentation.
+      </div>
     </div>
+  </div>
+</div>
   )
 }
 
