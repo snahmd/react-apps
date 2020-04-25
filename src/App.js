727a0ba5b767be0727b0a2bb6b0869cc7a653448
snahmd <sanahmed7@gmail.com>
2020-04-25 23:58:59 +0200

    adding card

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import faker from 'faker';
-import Comment from './Comment'
+import Comment from './Comment';
+import Card from './Card';
 
 const App = () => {
   return(
-    <div className="ui comments">
+    <div className="ui comments" style={{
+      paddingTop: '10px',
+      paddingLeft: '15px'
+    }}>
+      <Card>
       <Comment name="Ahmed San" avatar={faker.image.avatar()} timeAgo="2" star="5" comment="React Super"/>
+
+      </Card>
+      <Card>
       <Comment name="John Donne" avatar={faker.image.avatar()} timeAgo="3" star="3" comment="React Wow"/>
+
+      </Card>
     </div>
   )
 }
